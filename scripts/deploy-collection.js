569f9b7decec0dd93b359b8a7a4fc1abf8291162
Mitja Kjuder <mitja.kjuder@codeventuri.com>
2023-02-22 09:46:50 +0100

    fixes

--- scripts/deploy-collection.js
+++ scripts/deploy-collection.js
@@ -6,23 +6,26 @@ async function main() {
     "MockName",
     "MockSymbol",
     "https://api.example.com/nfts/",
-    "0x5f2B7077a7e5B4fdD97cBb56D9aD02a4f326896d", // Royalties address
-    false, // _isSoulbound
-    1000, // _maxSupply
-    50, // _reserve
+    "",
+    [false, false, false],
     hre.ethers.utils.parseUnits('0.001', 18).toString(), // _price
     0, // _dropStart
-
+    1000, // _maxSupply
+    50, // _reserve
+    "0x5f2B7077a7e5B4fdD97cBb56D9aD02a4f326896d", // Royalties address
+    5, // Royalties fees
     /*
-    string memory _name,
-    string memory _symbol,
-    string memory _initBaseURI,
-    address _royaltiesAddress,
-    bool _isSoulbound,
-    uint _maxSupply,
-    uint _reserve,
-    uint _price,
-    uint _dropStart
+        string memory _name,
+        string memory _symbol,
+        string memory _initBaseURI,
+        string memory _baseExtension,
+        bool[] memory _settings, [isDrop, isSoulbound, isRevokable]
+        uint _price,
+        uint _dropStart,
+        uint _maxSupply,
+        uint _reserve,
+        address _royaltiesAddress,
+        uint _royaltiesFees
     */
   );
 
